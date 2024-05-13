5f76c587e8c07022ae2183c0905a819c37cddc13
halocean96 <halocean96@gmail.com>
2024-05-13 22:37:25 +0900

    Update: carousel animation 적용

--- src/sections/SplashScreen.tsx
+++ src/sections/SplashScreen.tsx
@@ -1,19 +1,55 @@
 import { isPlayingSplashScreenAtom } from "@/atoms/splash";
 import { H1 } from "@/components/Typography";
 import { useAtom } from "jotai";
-import { useEffect } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { useEffect, useState } from "react";
+
+const ANIMATION_DELAY_TIME = 1000;
 
 const SplashScreen = () => {
+  const [api, setApi] = useState<CarouselApi>();
   const [_, setIsPlayingSplashScreen] = useAtom(isPlayingSplashScreenAtom);
   useEffect(() => {
-    setTimeout(() => {
-      setIsPlayingSplashScreen(false);
-    }, 2000);
-  }, [setIsPlayingSplashScreen]);
+    if (!api) return;
+    // @ts-ignore
+    api.on("autoplay:stop", () => {
+      setTimeout(() => {
+        setIsPlayingSplashScreen(false);
+      }, ANIMATION_DELAY_TIME);
+    });
+  }, [api, setIsPlayingSplashScreen]);
   return (
-    <div className="flex justify-center items-center absolute top-0 left-0 w-screen h-screen">
-      <H1>Splash screen test</H1>
-    </div>
+    <Carousel
+      setApi={setApi}
+      plugins={[
+        Autoplay({
+          delay: ANIMATION_DELAY_TIME,
+          stopOnLastSnap: true,
+          stop: () => {
+            console.log("auto play stop!!");
+          },
+        }),
+      ]}
+      className="flex justify-center bg-white items-center absolute top-0 left-0 w-screen h-screen"
+    >
+      <CarouselContent>
+        <CarouselItem>
+          <H1 className="text-center">1 페이지</H1>
+        </CarouselItem>
+        <CarouselItem>
+          <H1 className="text-center">2 페이지</H1>
+        </CarouselItem>
+        <CarouselItem>
+          <H1 className="text-center">3 페이지</H1>
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
   );
 };
 
